8ec8a2dd9e90bf3d73a5d4ccad3ccc86c8650470
ekrez <84871963+ekrez@users.noreply.github.com>
2024-04-17 11:35:32 +0300

    Add files via upload

---
+++ daiseg.mex.py
@@ -0,0 +1,140 @@
+import argparse
+import argparse
+import numpy as np
+import HMMmex as hmm
+import EMmex as EM
+
+
+
+parser = argparse.ArgumentParser(description='DAIseg') 
+
+parser.add_argument('--EM', type=str, help='Whether or not to use EM algorithm')
+parser.add_argument('--HMM_par', type= str, help='File with parameters')
+parser.add_argument('--o', type= str, help = 'Name of output file' )
+parser.add_argument('--EM_est', type= str, help = 'Make estimation of the all parameters or only coalescent times' )
+
+
+args = parser.parse_args()
+
+
+N = 5 # number of hidden states
+
+
+f = open(args.HMM_par, 'r')
+GEN_time = float(f.readline())
+MU = float(f.readline())
+RR = float(f.readline())
+L = int(f.readline())
+
+seq_start, seq_end = f.readline().split(' ')
+seq_start = int(seq_start)
+seq_end = int(seq_end.replace('\n',''))
+
+
+Lambda_0=np.zeros(11)
+Lambda_0[1] = float(f.readline())/GEN_time*MU*L
+Lambda_0[2] = float(f.readline())/GEN_time*MU*L
+Lambda_0[0] = float(f.readline())/GEN_time*MU*L
+Lambda_0[3] = float(f.readline())/GEN_time*MU* L
+Lambda_0[4] = float(f.readline())/GEN_time*MU* L
+Lambda_0[5] = float(f.readline())/GEN_time*MU* L
+Lambda_0[6] = float(f.readline())/GEN_time*MU* L
+Lambda_0[9] = float(f.readline())
+Lambda_0[10] = Lambda_0[9]
+Lambda_0[7] = float(f.readline())
+Lambda_0[8] = float(f.readline())
+
+f.close()
+
+d=MU*L
+
+
+seq0, seq1, seq2, seq3 = [], [], [], []
+with open('obs.eu.txt', "r") as f0, open('obs.na.txt', "r") as f1, open('obs.yri.txt', "r") as f2,open('obs.neand.txt', "r") as f3:
+    for line0, line1, line2, line3 in zip(f0, f1, f2, f3):
+    
+        row = line0.replace('\n','').split(' ')
+        row = [int(i) for i in row]
+        seq0.append(row)
+        
+        row = line1.replace('\n','').split(' ') 
+        row = [int(i) for i in row]
+        seq1.append(row)
+        
+        row = line2.replace('\n','').split(' ')
+        row = [int(i) for i in row]
+        seq2.append(row)
+
+        row = line3.replace('\n','').split(' ')
+        row = [int(i) for i in row]
+        seq3.append(row)
+
+seq0=np.array(seq0)
+seq0 = np.transpose(seq0)        
+seq1=np.array(seq1)
+seq1 = np.transpose(seq1)
+seq2=np.array(seq2)
+seq2 = np.transpose(seq2)
+seq3=np.array(seq3)
+seq3 = np.transpose(seq3)
+
+
+
+n0=seq0.max()
+n1=seq1.max()
+n2=seq2.max()
+n3=seq3.max()
+
+
+seq=[]
+for i in range(len(seq1)):
+    seq.append(np.column_stack((seq0[i],seq1[i], seq2[i], seq3[i])))   
+
+SEQ=np.array(seq)
+N_st=SEQ.max()+1
+
+P=[0.4, 0.05, 0.4, 0.05, 0.1]
+n_EM_steps = 10
+epsilon = 1e-10
+bnds=((0.5*Lambda_0[5], 2*Lambda_0[5]), (0.5*Lambda_0[6], 2*Lambda_0[6]))
+
+
+
+def run_daiseg(lmbd_opt,seq, n_st, idx):
+    d = MU * L       
+    lmbd_opt=Lambda_0        
+    a = hmm.initA(lmbd_opt[5]/d, lmbd_opt[6]/d, RR, L, lmbd_opt[7],  lmbd_opt[8],  lmbd_opt[9],  lmbd_opt[10])
+    b=hmm.initB(MU, L, lmbd_opt[0:5], n_st) 
+    tracts_HMM =  hmm.get_HMM_tracts(hmm.viterbi(seq [idx], P, a, b))
+    for k in range(N):
+        for j in range(len(tracts_HMM[k])):
+            tracts_HMM[k][j][0]= L * tracts_HMM[k][j][0] +seq_start
+            tracts_HMM[k][j][1]= L * (tracts_HMM[k][j][1]+1)-1 +seq_start
+
+    return tracts_HMM
+    
+    
+def EM_function(seq, lambda_0, n_st):
+
+    Lambda_new = EM.EM_algorithm(P, seq, n_st, MU, RR, Lambda_0, epsilon, L, bnds)
+    return Lambda_new
+
+tracts_HMM_result = []
+if args.EM=='no':
+
+    for idx in range(0, len(seq)):
+        tracts_HMM_result.append(run_daiseg(Lambda_0, SEQ, N_st, idx))
+
+
+
+        
+if args.EM=='yes': 
+    for idx in range(0, len(seq)):             
+        Lambda_opt = EM_function(SEQ[idx], Lambda_0, N_st)        
+        tracts_HMM_result.append(run_daiseg(Lambda_opt, SEQ, N_st, idx)) 
+        
+
+        
+with open(args.o, "w") as f:
+   for i in tracts_HMM_result:
+       f.write(str(i)+'\n') 
---
+++ obs.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+
+
+f_obs = "obs.chr22.ingroup.txt"
+
+f_neand = "chr22.archaic.reference.txt"
+f_yri = "chr22.yri.reference.txt"
+f_na= "chr22.na.reference.txt"
+f_eu = "chr22.eu.reference.txt"
+
+
+
+def make_obs(lines, lines_ref, L, ind):
+
+    start, end = lines_ref[0,0], lines_ref[-1,0]
+    if float(end-start) % L != 0:
+        T = int((end-start)/ L) + 1
+    obs_ref = np.zeros(T, int)
+    
+    obs = lines[:,ind]
+    
+    for i in range(len(lines)):
+        j=int((lines_ref[i][0]-start)/L)
+
+        if obs[i]==0:
+            if lines_ref[i][1]==-1:
+                obs_ref[j]+=1
+        if obs[i]==1:
+            if lines_ref[i][2]==-1:
+                obs_ref[j]+=1
+    return obs_ref
+
+
+with open(f_obs,'r') as f:
+    
+    
+    lines = f.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip(' \n').split(' ')
+
+        for j in range(len(lines[i])):
+            lines[i][j]=int(lines[i][j])
+lines = np.array(lines)  
+
+    
+    
+   
+with open(f_neand,'r') as f:
+    lines_neand = f.readlines()
+    
+with open(f_yri,'r') as f:
+    lines_yri = f.readlines()
+
+with open(f_eu,'r') as f:
+    lines_eu = f.readlines()
+    
+with open(f_na,'r') as f:
+    lines_na = f.readlines()
+        
+    
+for i in range(len(lines_yri)):
+    lines_yri[i] = lines_yri[i].strip('\n').split('\t')
+    for j in range(len(lines_yri[i])):
+        lines_yri[i][j]=int(lines_yri[i][j])
+        
+print(lines_yri[0:5])
+        
+        
+        
+for i in range(len(lines_eu)):
+    lines_eu[i] = lines_eu[i].strip('\n').split('\t')
+    for j in range(len(lines_eu[i])):
+        lines_eu[i][j]=int(lines_eu[i][j])
+        
+for i in range(len(lines_na)):
+    lines_na[i] = lines_na[i].strip('\n').split('\t')
+    for j in range(len(lines_na[i])):
+        lines_na[i][j]=int(lines_na[i][j])
+        
+for i in range(len(lines_neand)):
+    lines_neand[i] = lines_neand[i].strip('\n').split('\t')
+    for j in range(len(lines_neand[i])):
+        lines_neand[i][j]=int(lines_neand[i][j])    
+        
+lines_yri = np.array(lines_yri)
+lines_eu = np.array(lines_eu)
+lines_na = np.array(lines_na)
+lines_neand = np.array(lines_neand)
+
+
+
+
+
+start, end = lines_yri[0,0], lines_yri[-1,0]
+
+n_mex = len(lines[0])
+
+SEQ=[]
+N_ST=[]
+L=1000
+
+MU=1.25e-8
+RR=1e-8
+gen_time=29
+
+T_NEAND_migration = 55000 #time of Neanderthal migration into Out_of_africa population
+T_NEAND_AMH = 650000 # split time between AMH and Neanderthal
+T_OOF_AF = 65700 # Out_of_Africa migration time
+T_AS_EU_split = 41997 #Asian European split
+T_AS_NA_split = 17000 # time of crossing Beringian (Nielsen et al., 2017; Mendes et al., 2020)
+T_MEX_admix = 466 
+
+with open('par.file.txt', "w") as file_pos:
+    file_pos.write(str(gen_time)+'\n')
+    file_pos.write(str(MU)+'\n')
+    file_pos.write(str(RR)+'\n')
+    file_pos.write(str(L)+'\n')
+    file_pos.write(str(lines_yri[0,0])+' ' +str(lines_yri[-1,0])+'\n')
+    file_pos.write(str( T_NEAND_AMH )+'\n')
+    file_pos.write(str(T_OOF_AF)+'\n')
+    file_pos.write(str( T_NEAND_migration )+'\n')
+    file_pos.write(str( T_AS_EU_split )+'\n')
+    file_pos.write(str( T_MEX_admix  )+'\n')
+    file_pos.write(str( T_NEAND_migration )+'\n')
+    file_pos.write(str( T_MEX_admix  )+'\n')
+    file_pos.write(str(0.025)+'\n')
+    file_pos.write(str(0.45)+'\n')
+    file_pos.write(str(0.45)+'\n')
+    file_pos.write(str(0.1))
+        
+
+for ind in range(n_mex):
+    sq=np.vstack([make_obs(lines, lines_eu, L, ind ),make_obs(lines, lines_na, L, ind ), make_obs(lines, lines_yri, L, ind ),make_obs(lines, lines_neand, L, ind)])
+    sq=sq.transpose()
+    n_st = sq.max()+1
+    SEQ.append(sq)
+    N_ST.append(n_st)
+SEQ=np.array(SEQ)
+
+
+
+
+
+with open('obs.eu.txt', "w") as file1, open('obs.na.txt', "w") as file2, open('obs.yri.txt', "w") as file3,open('obs.neand.txt', "w") as file4:
+    for j in range(len(SEQ[0])):
+        s1, s2, s3, s4 = '','', '', ''
+        for i in range(n_mex):
+            s1 += str(SEQ[:,j,0][i])+' '
+            s2 += str(SEQ[:,j,1][i])+' '
+            s3 += str(SEQ[:,j,2][i])+' '
+            s4 += str(SEQ[:,j,3][i])+' '                
+        file1.write(s1[:-1]+'\n')
+        file2.write(s2[:-1]+'\n')
+        file3.write(s3[:-1]+'\n')
+        file4.write(s4[:-1]+'\n')       
+        
