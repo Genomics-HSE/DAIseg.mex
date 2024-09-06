a3229bac76eef535c24dcf0ea500cb2f095f3631
ekrez <84871963+ekrez@users.noreply.github.com>
2024-09-06 10:52:00 +0300

    Update useful2.py

--- useful2.py
+++ useful2.py
@@ -20,7 +20,30 @@ def point_in_set(p, m_a):
             return f
     return f
 
+# two set of intervals intersection 
+def intersections(a,b):
+    ranges = []
+    i = j = 0
+    while i < len(a) and j < len(b):
+        a_left, a_right = a[i]
+        b_left, b_right = b[j]
 
+        if a_right < b_right:
+            i += 1
+        else:
+            j += 1
+
+        if a_right >= b_left and b_right >= a_left:
+            end_pts = sorted([a_left, a_right, b_left, b_right])
+            middle = [end_pts[1], end_pts[2]]
+            ranges.append(middle)
+
+    ri = 0
+    while ri < len(ranges)-1:
+        if ranges[ri][1] == ranges[ri+1][0]:
+            ranges[ri:ri+2] = [[ranges[ri][0], ranges[ri+1][1]]]
+        ri += 1
+    return ranges
 
 def read_bed(f):
 
