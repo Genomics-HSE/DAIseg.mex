4cfab21aa4d5f854704c46f7d3653ff388001a71
ekrez <84871963+ekrez@users.noreply.github.com>
2024-09-11 11:06:26 +0300

    Update aa.hg19.sh

--- hg19/aa.hg19.sh
+++ hg19/aa.hg19.sh
@@ -9,9 +9,13 @@ do
 
 VCF1000=/media/scglab/T7/Work/data/1000GP/${i}/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz
 bcftools query -f '%POS %REF %ALT %INFO\n' ${VCF1000}> ./${dir}/POS.REF.ALT.INFO.chr${i}.txt
-done
+
 
 python3 Ancestral.Alleles.py ${CHR}
 
 rm ./${dir}/POS.REF.ALT.INFO.chr${i}.txt
+
+
+done
+
 
