8008927895331e6f2f520374630c80314eda3d01
ekrez <84871963+ekrez@users.noreply.github.com>
2024-09-24 17:37:15 +0300

    Add files via upload

--- preparations/obs2.py
+++ preparations/obs2.py
@@ -14,7 +14,7 @@ dct_all = usfl.main_read(f, f_aa)
 
 
 L=1000
-
+k=0
 with open(f_out,'w') as f1:
     f1.write('#POSITIONS\t#REF\t#ALT\t#ANCESTRAL\t#EU\t#NA\t#AF\t#ARCHAIC\t#MEX\n')
     for i  in dct_all.keys():
@@ -26,15 +26,28 @@ with open(f_out,'w') as f1:
         if s4=='':
             s4+='.'
         s5=str(j['Obs']).replace('[','').replace(']','').replace(',','')
-        f1.write(str(i)+'\t'+str(j['REF'])+'\t'+str(j['ALT'])+'\t'+str(j['AA'])+'\t'+s1+'\t'+s2+'\t'+s3+'\t'+s4+'\t'+s5+'\n')
-
-
-
-
-
-
-
-
+        
+        
+        flag=[]
+        if j['Archaic']==[]:
+            for o in set(j['Obs']):
+                if (o in set(j['EU'])) and  (o in set(j['NA'])) and  (o in set(j['AF'])):
+                    flag.append(True)
+            if flag==[True for o in set(j['Obs'])]:
+                k+=1
+            else:
+                f1.write(str(i)+'\t'+str(j['REF'])+'\t'+str(j['ALT'])+'\t'+str(j['AA'])+'\t'+s1+'\t'+s2+'\t'+s3+'\t'+s4+'\t'+s5+'\n') 
+        else:
+            for o in set(j['Obs']):
+                if (o in set(j['EU'])) and  (o in set(j['NA'])) and  (o in set(j['AF'])) and (o in set(j['Archaic'])):
+                    flag.append(True)
+            if flag==[True for o in set(j['Obs'])]:
+                k+=1
+            else:
+                f1.write(str(i)+'\t'+str(j['REF'])+'\t'+str(j['ALT'])+'\t'+str(j['AA'])+'\t'+s1+'\t'+s2+'\t'+s3+'\t'+s4+'\t'+s5+'\n')      
+        
+        
+        
 
 
 
